d36c792aa6cadbf9e54becf2dda59d8d2be7c7d9
Rijaia <zaya_89@mail.ru>
2023-08-14 15:39:08 +0300

    Homework number four (course three) SQL path 2

--- scripts.sql
+++
@@ -1,5 +0,0 @@
-select * from student where age>10 and student.age<20;
-select name from student;
-select * from student where name like '%O%';
-select * from student where age<id;
-select *from student order by age;
